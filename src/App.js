73fd5fa8af5b1a4437c703499fb7bf6e38973649
SBhanushali <shivam.n.bhanushali@gmail.com>
2020-09-16 21:15:36 +0530

    Setup routes

--- src/App.js
+++ src/App.js
@@ -1,11 +1,18 @@
 import React from "react";
-import { Box, Grid } from "@chakra-ui/core";
+import { Grid } from "@chakra-ui/core";
 import ModuleNavigation from "./Components/ModuleNavigation/ModuleNavigation";
+import { Switch, Route } from "react-router-dom";
+import Navbar from "./Components/Navbar/Navbar";
+import AttemptQuiz from "./Components/AttemptQuiz/AttemptQuiz";
 
 function App() {
   return (
     <Grid gridTemplateColumns="3fr 10fr">
+      <Navbar />
       <ModuleNavigation />
+      <Switch>
+        <Route path="/:moduleName" children={<AttemptQuiz />} />
+      </Switch>
     </Grid>
   );
 }
