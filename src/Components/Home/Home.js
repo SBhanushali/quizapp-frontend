859d0e23c32d4fe14fa7060493eb8f66d82afbec
SBhanushali <shivam.n.bhanushali@gmail.com>
2020-09-18 21:02:15 +0530

    Add loader

--- src/Components/Home/Home.js
+++ src/Components/Home/Home.js
@@ -1,16 +1,18 @@
 import React, { useEffect, useState } from "react";
 import { NavLink } from "react-router-dom";
-import { SimpleGrid } from "@chakra-ui/core";
+import { SimpleGrid, Skeleton } from "@chakra-ui/core";
 import Module from "./Module";
 
 const Home = () => {
   const [modules, setModules] = useState([]);
+  const [isLoaded, setLoading] = useState(false);
   useEffect(() => {
     fetch("https://apiquizbytes.herokuapp.com/modules", { method: "GET" })
       .then((response) => response.json())
       .then((result) => {
         if (result.data && result.success) {
           setModules(result.data);
+          setLoading(true);
         }
       })
       .catch((error) => {});
@@ -22,20 +24,37 @@ const Home = () => {
         to={`/${module.moduleName}`}
         activeStyle={{ color: "red", fontWeight: "bold" }}
       >
+        {" "}
         <Module moduleName={module.moduleName} />
       </NavLink>
     );
   });
   return (
-    <SimpleGrid
-      columns={[1, 2, null, 4]}
-      spacingY="40px"
-      spacingX="20px"
-      pt="10vh"
-      px={5}
-    >
-      {moduleMap}
-    </SimpleGrid>
+    <>
+      {isLoaded ? (
+        <SimpleGrid
+          columns={[1, 2, null, 4]}
+          spacingY="40px"
+          spacingX="20px"
+          pt="10vh"
+          px={5}
+        >
+          {moduleMap}
+        </SimpleGrid>
+      ) : (
+        <SimpleGrid
+          columns={[1, 2, null, 4]}
+          spacingY="40px"
+          spacingX="20px"
+          pt="10vh"
+          px={5}
+        >
+          <Skeleton height="200px" my="10px" rounded="10px" />
+          <Skeleton height="200px" my="10px" rounded="10px" />
+          <Skeleton height="200px" my="10px" rounded="10px" />
+        </SimpleGrid>
+      )}
+    </>
   );
 };
 
